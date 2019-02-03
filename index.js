93cca8a69ce120b8ebf0bf2a2f7142700ea2288c
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-02-03 10:15:05 +0100

    Playing with tensors and variables

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// Shape of tensor with 4 rows and 2 columns
 const shape = [4, 2];
+
+//Feed data in tensor
 const data = tf.tensor([4, 6, 5, 9, 13, 25, 1, 57], shape);
-data.print();
+
+//set variable with zeros
+const data2 = tf.variable(tf.zeros([8]));
+
+//Print the data
+data2.print();
+
+//Assign new values but shape shud be same
+data2.assign(tf.tensor1d([4, 12, 5, 6, 56, 3, 45, 5]));
+
+//Print the data
+data2.print();
